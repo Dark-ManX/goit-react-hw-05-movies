46a17aea20eb1e4325138c87c8ca903a9a53649c
Dark-ManX <sergiosis@ukr.net>
2022-08-12 12:44:54 +0300

    Updates

---
+++ static/js/364.e4901379.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[364],{364:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("p",{children:"Sorry, but we don't know such adress"})}}}]);
+//# sourceMappingURL=364.e4901379.chunk.js.map
